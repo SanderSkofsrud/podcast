61f6b09b02f2fc77e5491636f58780210e868b03
eliastrana <eliastrana@gmail.com>
2024-11-14 14:25:00 +0100

    feat(frontend):Desktop application in next.js

---
+++ desktopadblock/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AudioUploader from "@/components/audioUploader";
+
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Ad Remover Audio Processor</h1>
+            <AudioUploader />
+        </div>
+    );
+};
+
+export default Home;
